36a5466e5ccc0088eb00b744759f6ccdd9ccbb8e
Jacob Flores <jacob.e.flores562@gmail.com>
2018-03-10 23:28:07 -0800

    with firebase

--- script.js
+++ script.js
@@ -8,23 +8,56 @@ var config = {
   };
   firebase.initializeApp(config);
 
+    var database = firebase.database()
+    var connectionsRef = database.ref("/connections")
+    var connectedRef = database.ref(".info/connected")
+
+        // When the client's connection state changes...
+    connectedRef.on("value", function(snap) {
+
+      // If they are connected..
+     if (snap.val()) {
+        // Add user to the connections list.
+        var con = connectionsRef.push(true)
+        // Remove user from the connection list when they disconnect.
+        con.onDisconnect().remove()
+      }
+    })
+
+    //Create a userProfile to capture this Player's info
+    var userProfile = new Object()
+
+    database.ref("/count").on("value", function(snapshot){
+      if (snapshot.child("Count").exists()){
+          userCount = parseInt(snapshot.val().Count)
+          console.log("user Count: " + userCount)
+      }
+      else {
+          userCount = 0
+          database.ref("/count").set({
+              Count: userCount
+          })
+      }
+  })
 
     // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
     var wins = 0
     var losses = 0
-    var ties = 0;
+    var ties = 0
     
-    var select = document.getElementById("choice")
-    var reminder = document.getElementById("remind")
-    var winRate = document.getElementById("win");
-    var lossRate = document.getElementById("loss");
-    var tieRate = document.getElementById("tie");
+    var select = $("#choice")
+    var reminder = $("#remind")
+    var winRate = $("#win")
+    var lossRate = $("#loss")
+    var tieRate = $("#tie")
 
-    document.getElementById("button").addEventListener("click", function(){
+    $("#play").on("click", function(){
       
-      alert("Let The Games Begin. Select r for rock, s for scissors, or p for paper")
       reminder.textContent = "Please select r, p, or s"
-      var computerChoices = ["r", "p", "s"]
+      // $("#button").append("<h2>"+reminder.textContent+"</h2>")
+      console.log("hit")
+      
+      prompt("Please select R, P, or S")
 
       // This function is run whenever the user presses a key.
       document.onkeyup = function(event) {
@@ -32,6 +65,11 @@ var config = {
 
         // Determines which key was pressed.
         var userGuess = event.key
+        console.log(userGuess)
+        //Push to firebase
+        database.ref().push({
+          Choice: userGuess
+      }) 
         
         
         
@@ -40,7 +78,6 @@ var config = {
           alert("Please only input r, p, or s")
         }
         // Randomly chooses a choice from the options array. This is the Computer's guess.
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
 
         if(userGuess === "r" || userGuess === "p" || userGuess === "s"){
           reminder.textContent = null
@@ -100,10 +137,30 @@ var config = {
         }
 
         winRate.textContent = wins;
+        //Push to firebase
+        database.ref().push({
+          Wins: wins,
+          Losses: losses,
+          Ties: ties
+      }) 
         lossRate.textContent = losses;
         tieRate.textContent = ties;
 
       }
     })
+
+
+
+database.ref().on("value", function(snapshot){
+  console.log(snapshot.val())
+  userArr = snapshot.val()
+  newArray = []
+  Object.keys(userArr).map(function(key) {
+      newArray.push([userArr[key]])
+      // console.log(newArray)
+  })
+  console.log(newArray[1][0].Profile)
+  
+})
 
   
